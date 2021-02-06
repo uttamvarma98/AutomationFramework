600346641fba5a172af54c9844cd9086836687c6
uttamvarma98 <uttamkumar.vummididevu@zenq.com>
2021-02-06 11:31:36 +0530

    Six

--- src/test/java/Calculator/Operations.java
+++ src/test/java/Calculator/Operations.java
@@ -3,13 +3,13 @@ package Calculator;
 public class Operations {
 
     String action;String result;
-    //int operand1,operand2;
+    
     Operations(String action){
         this.action=action;
     }
 
     public String execute(int operand1,int operand2) {
-        //this.operand1=operand1;this.operand2=operand2;//Initializing user given values
+
         switch(action) {
             case "add":
                 result=String.valueOf(operand1+operand2);
