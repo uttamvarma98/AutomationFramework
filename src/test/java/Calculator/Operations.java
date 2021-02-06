feeba2502f5ad9986522aedcb62ac1e2f5b66414
uttamvarma98 <uttamkumar.vummididevu@zenq.com>
2021-02-06 11:12:01 +0530

    Imp

--- src/test/java/Calculator/Operations.java
+++ src/test/java/Calculator/Operations.java
@@ -1,4 +1,35 @@
 package Calculator;
 
 public class Operations {
+
+    String action;String result;
+    //int operand1,operand2;
+    Operations(String action){
+        this.action=action;
+    }
+
+    public String execute(int operand1,int operand2) {
+        //this.operand1=operand1;this.operand2=operand2;//Initializing user given values
+        switch(action) {
+            case "add":
+                result=String.valueOf(operand1+operand2);
+                break;
+            case "sub":
+                result=String.valueOf(operand1-operand2);
+                break;
+            case "mul":
+                result=String.valueOf(operand1*operand2);
+
+                break;
+            case "div":
+                result=String.valueOf(operand1/operand2);
+                break;
+        }
+
+        return result;
+
+    }
+
+
+
 }
