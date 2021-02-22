7a9d8b8c38ef5ab2e5e4266da0f92bb65bbf10d5
uttam.vummididevu <uttamkumar.vummididevu@zenq.com>
2021-02-22 10:24:45 +0530

    Used Return Types in Business Logic

--- src/test/java/sauceLabsAssignment/BusinessLogicForAutomatingAddToCart.java
+++ src/test/java/sauceLabsAssignment/BusinessLogicForAutomatingAddToCart.java
@@ -6,11 +6,18 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BusinessLogicForAutomatingAddToCart extends ExecutableForAutomatingAddToCart {
     private String username,password;
-    BusinessLogicForAutomatingAddToCart(String username1,String password1) {
-        this.username = username1;
-        this.password = password1;
+    BusinessLogicForAutomatingAddToCart(String username1,String password1) throws InterruptedException {
+        if(username1.length()<=15&&password1.length()<=15) {
+            this.username = username1;
+            this.password = password1;
+            //login();
+        }
+        else{
+            System.out.println("Dear user please enter valid credentials");
+            System.exit(1);
+        }
     }
-    public String login() throws InterruptedException {
+        public String login() throws InterruptedException {
         chromeBrowser();
         driver.get("https://www.saucedemo.com/index.html");
 
@@ -21,25 +28,28 @@ public class BusinessLogicForAutomatingAddToCart extends ExecutableForAutomating
         driver.findElement((By.cssSelector("#password"))).sendKeys(password);
 
         driver.findElement(By.cssSelector("#login-button")).click();
-        addTheProductToCart();
-        return  "Automating Add To Cart Feature Is Done Successfully";
+        //addTheProductToCart();
+        return  "Automating Add To Cart Is Done Successfully";
     }
-    private void addTheProductToCart() throws InterruptedException {
+    public String addTheProductToCart() throws InterruptedException {
 
         driver.findElement(By.xpath("//*[@id=\"inventory_container\"]/div/div[1]/div[3]/button")).click();
         driver.findElement(By.cssSelector("#shopping_cart_container > a > svg > path")).click();
-        identifyProductInCart();
+        //identifyProductInCart();
+        return "Product adding to cart is done successfully";
     }
-    private void identifyProductInCart() throws InterruptedException{
+    public String identifyProductInCart() throws InterruptedException{
         WebDriverWait wait=new WebDriverWait(driver,15);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[2]/div[3]/div/div[1]/div[3]/div[2]/a/div")));
         Thread.sleep(2000);
         System.out.println("Item located In Cart");
-        browserTerminator();
+       // browserTerminator();
+        return "Product is identified in the cart";
     }
-    private void browserTerminator(){
+    public String browserTerminator(){
 
         driver.quit();
+        return "Closed the browser succesfully";
 
     }
 }
