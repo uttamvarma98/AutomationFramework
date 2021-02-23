d172166fb086c8028de725b9faf300cf0307cabc
uttam.vummididevu <uttamkumar.vummididevu@zenq.com>
2021-02-23 23:39:14 +0530

    adds quit method for closing driver

--- src/test/java/pageObjectModelDemo/LoginPageOfSauceLabs.java
+++ src/test/java/pageObjectModelDemo/LoginPageOfSauceLabs.java
@@ -3,15 +3,16 @@ package pageObjectModelDemo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import sauceLabsAssignment.BrowserSetUp;
 
 @Test
-public class LoginPageOfSauceLabs {
-    WebDriver driver;
+public class LoginPageOfSauceLabs extends BrowserSetUp {
+    //WebDriver driver;
     public void verifyValidLogin(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+       /* System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
          driver=new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/index.html");
+        */
         LocatorsOfSauceLabs locatorsOfSauceLabs=new LocatorsOfSauceLabs(driver);
         locatorsOfSauceLabs.typeUserName();
         locatorsOfSauceLabs.typePassword();
