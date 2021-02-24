e12b74f6c0640291740a9aa29449660b0412b343
uttam.vummididevu <uttamkumar.vummididevu@zenq.com>
2021-02-24 17:29:56 +0530

    Uses xml file for passing parameters

--- src/test/java/sauceLabsAssignment/AutomateProductAddToCartInSauceLabs.java
+++ src/test/java/sauceLabsAssignment/AutomateProductAddToCartInSauceLabs.java
@@ -1,13 +1,17 @@
 package sauceLabsAssignment;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class AutomateProductAddToCartInSauceLabs extends BrowserSetUp {
     @Test
-    public void automateProductAddToCart() {
+    @Parameters({"usernameOfUser","passwordOfUser"})
+    public void automateProductAddToCart(String usernameOfUser,String passwordOfUser ) {
+        //String usernameOfUser="standard_user",passwordOfUser="secret_sauce";
         LocatorsAutomateAddToCart locatorsAutomateAddToCart = new LocatorsAutomateAddToCart(driver);
-        locatorsAutomateAddToCart.typeUsername("standard_user");
-        locatorsAutomateAddToCart.typePassword("secret_sauce");
+        driver.get("https://www.saucedemo.com/index.html");
+        locatorsAutomateAddToCart.typeUsername(usernameOfUser);
+        locatorsAutomateAddToCart.typePassword(passwordOfUser);
         locatorsAutomateAddToCart.clickLoginButton();
         locatorsAutomateAddToCart.clickAddToCart();
         locatorsAutomateAddToCart.clickCartContainer();
--- src/test/java/sauceLabsAssignment/BrowserSetUp.java
+++ src/test/java/sauceLabsAssignment/BrowserSetUp.java
@@ -2,21 +2,32 @@ package sauceLabsAssignment;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class BrowserSetUp {
 
     public WebDriver driver;
-@BeforeMethod
-    public void chromeBrowser(){
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void browserLaunch(String browser){
+        if(browser.equalsIgnoreCase("ChromeBrowser")) {
+            System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
 
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+            driver = new ChromeDriver();
 
-        driver=new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        if(browser.equalsIgnoreCase("FirefoxBrowser")) {
+            System.setProperty("webdriver.gecko.driver","resources/geckodriver.exe");
 
-        driver.manage().window().maximize();
+            driver = new FirefoxDriver();
 
+            driver.manage().window().maximize();
+        }
 
     }
 }
