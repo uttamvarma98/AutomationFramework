53253316c1fa7e423c6220cdd7e0afa3627f4619
uttam.vummididevu <uttamkumar.vummididevu@zenq.com>
2021-02-25 21:55:49 +0530

    Adds CacheLookup for decreasing runtime

--- src/main/java/pageObjectModelDemo/LoginPageOfSauceLabs.java
+++ src/main/java/pageObjectModelDemo/LoginPageOfSauceLabs.java
@@ -1,16 +1,19 @@
 package pageObjectModelDemo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import swagLabsPackage.BrowserSetUp;
+import swagLabsPackage1.BrowserSetUp;
 
 @Test
-public class LoginPageOfSauceLabs extends BrowserSetUp {
-    //WebDriver driver;
+public class LoginPageOfSauceLabs extends BrowserSetUp  {
+   // WebDriver driver;
     public void verifyValidLogin(){
        /* System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
          driver=new ChromeDriver();
         driver.manage().window().maximize();
-        */
+        driver.get("https://www.saucedemo.com/");
+*/
         LocatorsOfSauceLabs locatorsOfSauceLabs=new LocatorsOfSauceLabs(driver);
         locatorsOfSauceLabs.typeUserName();
         locatorsOfSauceLabs.typePassword();
--- src/main/java/swagLabsPackage1/LocatorsAutomateAddToCart1.java
+++ src/main/java/swagLabsPackage1/LocatorsAutomateAddToCart1.java
@@ -1,24 +1,27 @@
-package swagLabsPackage;
+package swagLabsPackage1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LocatorsAutomateAddToCart {
+public class LocatorsAutomateAddToCart1 {
     WebDriver driver;
-    By user_name=By.id("user-name");
+    By user_name=By.cssSelector("input[type='text']");
     By pass_word=By.id("password");
     By loginButton=By.xpath("//input[@type='submit']");
     By addToCart=By.xpath("//div[@id=\"inventory_container\"]/div/div[1]//button");
     By cartContainer=By.cssSelector("path[fill='currentColor']");
     By backPack=By.xpath("//div[@class='inventory_item_name']");
+    By removeButton=By.xpath("//button[@class='btn_secondary cart_button']");
+    By continueShoppingButton=By.xpath("//a[@class='btn_secondary']");
+    By findProductsPage=By.cssSelector("div[class='peek']");
 
-    public LocatorsAutomateAddToCart(WebDriver driver){
+
+    public LocatorsAutomateAddToCart1(WebDriver driver){
         this.driver=driver;
     }
     public void typeUsername(String username){
-
         driver.findElement(user_name).sendKeys(username);
     }
     public void typePassword(String password){
